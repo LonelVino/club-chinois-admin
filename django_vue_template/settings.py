f8aeb463025403da40717014e4016bc450199f99
LonelVino <lonelvino@gmail.com>
2021-05-11 17:47:41 +0200

    new push test

--- django_vue_template/settings.py
+++ django_vue_template/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*7ri2430kc9-tw5p0*&(9ih5h-6fzf!6sra1!0qiqx=rf4iw^p'
+# SECRET_KEY = 'django-insecure-*7ri2430kc9-tw5p0*&(9ih5h-6fzf!6sra1!0qiqx=rf4iw^p'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
