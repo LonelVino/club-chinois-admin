7983bc060a643196ffc2a051bb0d26ab19e8d5aa
LonelVino <lonelvino@gmail.com>
2021-05-16 07:21:40 +0200

    change the settings of database

--- django_vue_template/settings.py
+++ django_vue_template/settings.py
@@ -119,16 +119,16 @@ WSGI_APPLICATION = 'django_vue_template.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'worldWeek.db',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'worldWeek.db',
+    }
+}
 
-## clear the DATABASES variable and then set the 'default' key using the dj_database_url module.
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# # clear the DATABASES variable and then set the 'default' key using the dj_database_url module.
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
